4b0e30efc7b9b53a2e72232e4147fdd5ad0fd436
Lars Gronvold <lars.gronvold@gmail.com>
2017-03-03 09:44:59 +0100

    bugfix: pmax() does not support long vectors, so apply it column-wise

--- R/CLR.R
+++ R/CLR.R
@@ -1,5 +1,8 @@
 calcCLR <- function(mi){
-  miZ <- pmax(scale(mi),0)^2
+  miZ <- scale(mi)
+  gc()
+  # pmax does not support long vectors so it is applied columnwise
+  miZ <- apply(miZ,2,pmax,0)^2
   gc()
   sqrt(miZ + t(miZ))
 }
