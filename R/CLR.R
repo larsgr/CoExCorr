40d985ace413342a86ff28f8a44ce113ed33ae7d
Lars Gronvold <lars.gronvold@gmail.com>
2017-05-18 13:54:47 +0200

    CLR memory efficient and NA proof

--- R/CLR.R
+++ R/CLR.R
@@ -1,8 +1,35 @@
-# Note: This function does not support matrices larger than 2^31 and can use a lot of memory
+# # Note: This function does not support matrices larger than 2^31 and can use a lot of memory
+# calcCLR <- function(mi){
+#   miZ <- pmax(scale(mi),0,na.rm = T)^2
+#   sqrt(miZ + t(miZ))
+# }
+
+# memory efficient implementation, supports matrices larger than 2^31
 calcCLR <- function(mi){
-  miZ <- pmax(scale(mi),0,na.rm = T)^2
-  sqrt(miZ + t(miZ))
+  n <- ncol(mi)
+  
+  # calculate sum and standard deviation for each column
+  miColMean <- numeric(n)
+  miColSD <- numeric(n)
+  # for each column
+  for(i in 1:n){
+    miColMean[i] <- mean(mi[ ,i],na.rm = T)
+    miColSD[i] <- sd(mi[ ,i],na.rm = T)
+  }
+  
+  # create clr matrix with same dimensions and dimnames as mi
+  clr <- mi
+  
+  for(i in 1:n){
+    # note: clr[ ,i] is the same as mi[refOrthoIDs,i]
+    Zc <- (clr[ ,i] - miColMean[i])/miColSD[i]
+    Zr <- (clr[ ,i] - miColMean)/miColSD
+    clr[ ,i] <- sqrt(pmax(Zc,0,na.rm = T)^2 + pmax(Zr,0,na.rm = T)^2)
+  }
+  
+  return(clr)
 }
+
 
 # memory efficient implementation
 # returns only the rows given by refOrthoIDs
@@ -14,8 +41,8 @@ calcCLRref <- function(mi, refOrthoIDs){
   miColSD <- numeric(n)
   # for each column
   for(i in 1:n){
-    miColMean[i] <- mean(mi[ ,i])
-    miColSD[i] <- sd(mi[ ,i])
+    miColMean[i] <- mean(mi[ ,i],na.rm = T)
+    miColSD[i] <- sd(mi[ ,i],na.rm = T)
   }
   
   # only need the rows with ref.orthos
@@ -32,3 +59,4 @@ calcCLRref <- function(mi, refOrthoIDs){
   
   return(clr)
 }
+
